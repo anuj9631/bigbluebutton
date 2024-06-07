4b8665e42ce241c2075aebb4cb610f8928506558
ANUJ KUMAR <124524155+anuj9631@users.noreply.github.com>
2024-06-07 11:48:27 +0530

    Update chatRemove.ts
    Changed as String to as string. In TypeScript, the correct type for strings is string, not String. String refers to the String object, which is not the same as the primitive type string.
    
    Added as string to ensure chatId is treated as a string. This ensures type safety and consistency.
    
    Changed //TODO to // TODO for better readability and consistency with common code commenting practices.

--- bbb-graphql-actions/src/actions/chatRemove.ts
+++ bbb-graphql-actions/src/actions/chatRemove.ts
@@ -1,11 +1,11 @@
 import { RedisMessage } from '../types';
 
 export default function buildRedisMessage(sessionVariables: Record<string, unknown>, input: Record<string, unknown>): RedisMessage {
-  const eventName = `DestroyGroupChatReqMsg`;
+  const eventName = 'DestroyGroupChatReqMsg';
 
   const routing = {
-    meetingId: sessionVariables['x-hasura-meetingid'] as String,
-    userId: sessionVariables['x-hasura-userid'] as String
+    meetingId: sessionVariables['x-hasura-meetingid'] as string,
+    userId: sessionVariables['x-hasura-userid'] as string
   };
 
   const header = { 
@@ -15,10 +15,10 @@ export default function buildRedisMessage(sessionVariables: Record<string, unkno
   };
 
   const body = {
-    chatId: input.chatId,
+    chatId: input.chatId as string,
   };
 
-  //TODO Akka-apps doesn't handle this message
+  // TODO: Akka-apps doesn't handle this message
 
   return { eventName, routing, header, body };
 }
